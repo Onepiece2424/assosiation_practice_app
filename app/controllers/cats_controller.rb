5169eb37b22ce75e6292a6a01adc69363366e443
rioto.nagano <kingdom950927@gmail.com>
2022-10-02 15:39:06 +0900

    includesメソッドを使用しN+1問題を解決

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,7 +1,6 @@
 class CatsController < ApplicationController
   def index
-    @cats = Cat.all
-    @owners = Owner.all
+    @cats = Cat.includes(:owner)
     @owner_cat_joins = Owner.joins(:cats)
     @cat_owner_joins =  Cat.joins(:owner)
     @cat_childs = CatChild.all
