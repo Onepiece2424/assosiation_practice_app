9be7d79bbb4f0d40f3590c5be2398e2a6871f5c6
rioto.nagano <kingdom950927@gmail.com>
2022-10-02 21:05:29 +0900

    不定な引数の時のメソッド定義の方法と出力

--- myloop.rb
+++ myloop.rb
@@ -1,12 +1,37 @@
-def myloop
-  while true
-    yield      # ブロックを実行する
-  end
+# ブロック付きメソッド
+# def myloop
+#   while true
+#     yield      # ブロックを実行する
+#   end
+# end
+
+# num = 1       # numの初期化
+# myloop do
+#   puts "num is #{num}"    # numの表示
+#   break if num > 10       # numが10を超えた時に抜ける
+#   num *= 2                # numを2倍にする
+# end
+
+# 引数の数が不定なメソッドの書き方
+def foo(*args)
+  args
 end
 
-num = 1       # numの初期化
-myloop do
-  puts "num is #{num}"    # numの表示
-  break if num > 10       # numが10を超えた時に抜ける
-  num *= 2                # numを2倍にする
+p foo(1, 2, 3)
+
+# 設定した引数の数でない時にメソッドを実行した時
+def meth (arg, *args)
+  [arg, args]
 end
+
+p meth(1)
+p meth(1, 2, 3).flatten
+p meth([], 1)
+p meth(0, 1)
+
+def a(a, *b, c)
+  [a, b, c]
+end
+
+p a(1, 2, 3, 4, 5)
+p a(1, 2)
