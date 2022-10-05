1a0dfca764c50555c902a9e5333fd4317f09294d
rioto.nagano <kingdom950927@gmail.com>
2022-10-06 07:04:07 +0900

    classメソッドの作成

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,4 +2,12 @@ class Owner < ApplicationRecord
   has_many :cats, dependent: :destroy
   has_many :dogs, dependent: :destroy
   scope :publish, -> {where(cats: { name: "モモ" }, name: "田中")}
+
+  class << self
+    def owner_name
+      title = "classメソッドの使用"
+      ret = "Ownerだよ!"
+      p title + ret
+    end
+  end
 end
