d41e70255c236de3d6b02c911d5f0e90d3448572
rioto.nagano <kingdom950927@gmail.com>
2022-10-10 10:05:13 +0900

    単項演算子を使用し演算子の再定義

--- point.rb
+++ point.rb
@@ -16,8 +16,21 @@ class Point
   def -(other) # x, yをそれぞれひく
     self.class.new(x - other.x, y - other.y)
   end
+
+  def +@
+    dup  # 自分の複製を返す
+  end
+
+  def -@
+    self.class.new(-x, -y) # x, yのそれぞれの正負を逆にする
+  end
+
+  def ~@
+    self.class.new(-y, -x) # 90度反転させた座標を返す
+  end
 end
 
+# 二項演算子出力値
 point0 = Point.new(3, 6)
 point1 = Point.new(1, 8)
 
@@ -25,3 +38,9 @@ p point0
 p point1
 p point0 + point1
 p point0 - point1
+
+#単項演算子出力値
+point = Point.new(3, 6)
+p +point
+p -point
+p ~point
